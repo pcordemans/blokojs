67887b5d0dbc78368faf4d50bcf1ca6eb8a6a9b1
Sille Van Landschoot <info@sillevl.be>
2015-11-24 20:22:06 +0100

    add tests

--- bloko.js
+++ bloko.js
@@ -6,9 +6,17 @@ app.get('/', function (req, res) {
   res.send('Hello World!');
 });
 
+app.get('/about', function (req, res) {
+  res.send('About page');
+});
+
+app.get('/help', function (req, res) {
+  res.send('help page');
+});
+
 var server = app.listen(3000, function () {
   var host = server.address().address;
   var port = server.address().port;
-
-  console.log('Example app listening at http://%s:%s', host, port);
 });
+
+module.exports = server;
---
+++ test/pages.js
@@ -0,0 +1,33 @@
+var request = require('supertest');
+
+
+describe('basic pages', function(){
+
+  var server;
+
+  before(function(){
+    server = require('../bloko');
+  });
+
+  after(function(){
+    server.close();
+  });
+
+  it('get the home page at /', function(done){
+    request(server)
+      .get('/')
+      .expect(200, done);
+  });
+
+  it('get the about page at /about', function(done){
+    request(server)
+      .get('/about')
+      .expect(200, done);
+  });
+
+  it('get the help page at /help', function(done){
+    request(server)
+      .get('/help')
+      .expect(200, done);
+  });
+});
